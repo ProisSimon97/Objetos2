1ad1f05d1ae91eb887352bd59278c479241eb464
Simón <Simn@DESKTOP-4T7I4GD>
2021-04-22 20:05:39 -0300

    1.1.1

--- layering_2/src/Main/Main.java
+++ layering_2/src/Main/Main.java
@@ -11,30 +11,29 @@ import ar.unrn.tp4.persistencia.EnDiscoGuardarEmpleados;
 
 public class Main {
 	public static void main(String[] args) {
-		
+
 		PersistenciaEmpleados persistencia = new EnDiscoGuardarEmpleados();
 		EnvioCorreoElectronico envioEmail = new EnvioEmail();
-		
+
 		try {
 			Empleado empleado1 = new Empleado("Preuss", "Simon", LocalDate.of(1997, 04, 07), "simon@gmail");
 			Empleado empleado2 = new Empleado("Chubut", "Ana", LocalDate.of(2000, 12, 9), "ana@gmail");
-			
+
 			persistencia.guardarEmpleado(empleado1);
 			persistencia.guardarEmpleado(empleado2);
-			
+
 			String empleados = persistencia.recuperarEmpleados();
-			
-			if(!empleados.isEmpty()) {
+
+			if (!empleados.isEmpty()) {
 				envioEmail.enviar("Simon", "Aviso Final", "Ultimo aviso para retirar su tarjeta");
-				
+
 				System.out.println("pene");
 			}
-				
-			
+
 		} catch (CreateException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
+
 	}
 }
